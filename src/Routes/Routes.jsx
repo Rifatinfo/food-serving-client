19ad3be4e5f6994767ad2246d069324c5f612c7c
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-05-11 20:33:49 +0600

    page structure create

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "../Pages/Home";
+import Root from "../Root/Root";
+import ErrorPage from "../Pages/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:<Root/>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+        ],
+    },
+]);
+
+export default router;
